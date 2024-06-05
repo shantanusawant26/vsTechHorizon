c60bd22528b076656e7264d657c06b7fe62cd250
shantanusawant26 <shantanusawant26@gmail.com>
2024-06-05 17:56:06 +0530

    chages

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ app.use(function(req, res, next) {
 });
 
 // Define URL handler
-app.use("/", conatactUsRouters);
+app.use("/contact", conatactUsRouters);
 
 // Start the server
 const PORT = 3006;
